c3db205da39998b2c92feada07410459765a7944
Serge Manu <firstmanu25@yahoo.fr>
2025-07-18 20:20:13 +0000

    modif 18/07/25

--- django_invoice/settings.py
+++ django_invoice/settings.py
@@ -8,10 +8,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+try:
+    from  django.contrib.messages import constants as messages
+    MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger'
+    }
+except Exception as e:
+    pass
 SECRET_KEY = 'django-insecure-j@riz^4cyzor$8zfl^$ciw*dp(jd@i1wxba2#%v!_(^l(gzcp$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
